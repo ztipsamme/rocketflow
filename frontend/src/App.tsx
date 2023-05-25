d719d1dea8965a068fc58173dcf584a25520cd01
ztipsamme <e.spitz@hotmail.com>
2023-05-25 14:33:52 +0200

    Removed window-size funktion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,33 +13,9 @@ import Pomodoro from './views/Pomodoro'
 import NotFound from './views/NotFound'
 
 function Root() {
-    const [size, setSize] = useState<string>()
-
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            setSize(
-                `Window size: ${window.innerHeight}px ✕ ${window.innerWidth}px`
-            )
-            console.log(window.innerWidth)
-        })
-    })
     return (
         <div className="App">
             <nav className="navigation"></nav>
-            {/* <div
-                style={{
-                    position: 'absolute',
-                    margin: 0,
-                    padding: 0,
-                    inset: 0,
-                    textAlign: 'right',
-                    fontSize: '20px',
-                    color: 'black',
-                    fontFamily: 'Helvetica',
-                }}
-            >
-                {size}
-            </div> */}
             <Outlet />
             <footer></footer>
         </div>
